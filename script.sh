8ed44b4a2ac1ea6e96162bd7161b9987eacf2180
Alena Tesarova <xtesar36@stud.fit.vutbr.cz>
2021-04-22 23:40:29 +0200

    script updated

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Lets start..."
 
-RUNS=10
+RUNS=1
 statsFileName="statistics.txt"
 
 # Configuration no. 1
@@ -9,13 +9,29 @@ NEIGHBORHOOD=1
 NUM_CONFIG=2000
 CONFIG_LENGTH=11
 STEPS=20
-GENERATIONS=30
-POPSIZE=2000
+GENERATIONS=20
+POPSIZE=1000
 
-echo "Epoche;Neighborhood;Total configurations;Configuration length;Steps;Total generation;Population size;Fitness;Max fitness;Success;Best step;Best generation" > $statsFileName;
+echo "Epoche;Neighborhood;Total configurations;Configuration length;Steps;Total generation;Population size;Fitness;Max fitness;Success;Best step;Best generation;Rules" > $statsFileName;
 
 for (( i=0;i<RUNS;i++ ))
 do
     echo -n "$i;">> $statsFileName;
-    ./ca_major -n $NEIGHBORHOOD -c $NUM_CONFIG -l $CONFIG_LENGTH -s $STEPS -g $GENERATIONS -p $POPSIZE
+    ./ca_major -n $NEIGHBORHOOD -c $NUM_CONFIG -l $CONFIG_LENGTH -s $STEPS -g $GENERATIONS -p $POPSIZE;
+done
+
+NEIGHBORHOOD=$((NEIGHBORHOOD+1))
+
+for (( i=0;i<RUNS;i++ ))
+do
+    echo -n "$i;">> $statsFileName;
+    ./ca_major -n $NEIGHBORHOOD -c $NUM_CONFIG -l $CONFIG_LENGTH -s $STEPS -g $GENERATIONS -p $POPSIZE;
+done
+
+NEIGHBORHOOD=$((NEIGHBORHOOD+1))
+
+for (( i=0;i<RUNS;i++ ))
+do
+    echo -n "$i;">> $statsFileName;
+    ./ca_major -n $NEIGHBORHOOD -c $NUM_CONFIG -l $CONFIG_LENGTH -s $STEPS -g $GENERATIONS -p $POPSIZE;
 done
